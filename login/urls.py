e82c7a1741d8292613b6ada3c0b1cda4f89f3e52
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2022-03-12 13:52:58 +0700

    Added login page

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import signupPage
+from .views import signupPage,loginPage,logout
 
 urlpatterns = [
     path('signup/', signupPage, name='signup'),
-    # path('login/', loginPage, name='login'),
-    # path('logout/', logout, name='logout'),
+    path('login/', loginPage, name='login'),
+    path('logout/', logout, name='logout'),
 ]
