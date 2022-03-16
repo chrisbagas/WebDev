447a4b189080bc6d11902ba95648787c82f7ce13
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2022-03-16 22:13:18 +0700

    Update Blog Admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,12 +3,5 @@ from .models import Blog, Comments
 
 admin.site.register(Comments)
 
-class BlogAdmin(admin.ModelAdmin):
-    readonly_fields = ['creator']
-
-    def save_model(self, request, obj, form, change):
-        obj.creator = request.user
-        obj.last_modified_by = request.user
-        obj.save()
-admin.site.register(Blog, BlogAdmin)
+admin.site.register(Blog)
 # Register your models here.
