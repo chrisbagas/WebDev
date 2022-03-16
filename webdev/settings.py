68941c93e6b794a7e1f828178ad52a83b9362fc4
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2022-03-16 22:41:16 +0700

    Update settings and requirements

--- webdev/settings.py
+++ webdev/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
